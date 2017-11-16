60a7baebfc253bb9221a2e6ad030240c1d867096
Erolon <potionist1@gmail.com>
2017-11-16 09:26:50 +0200

    Started working on a fix for monster pathing

--- src/monster.py
+++ src/monster.py
@@ -14,24 +14,26 @@ class Monster:
         if not self.moveFrequency == self.moveCounter:
             self.moveCounter += 1
             return
-        if destination.x > self.position.x and self.isTileFreeForMonster(self.speed, 0, mapList):
+        if destination.x > self.position.x and self.isTileFreeForMonster(self.speed, 0, mapList, data_holder):
             self.position.x += self.speed
             self.moveCounter = 0
-        elif destination.x < self.position.x and self.isTileFreeForMonster(-self.speed, 0, mapList):
+        elif destination.x < self.position.x and self.isTileFreeForMonster(-self.speed, 0, mapList, data_holder):
             self.position.x -= self.speed
             self.moveCounter = 0
-        if destination.y > self.position.y and self.isTileFreeForMonster(0, self.speed, mapList):
+        if destination.y > self.position.y and self.isTileFreeForMonster(0, self.speed, mapList, data_holder):
             self.position.y += self.speed
             self.moveCounter = 0
-        elif destination.y < self.position.y and self.isTileFreeForMonster(0, -self.speed, mapList):
+        elif destination.y < self.position.y and self.isTileFreeForMonster(0, -self.speed, mapList, data_holder):
             self.position.y -= self.speed
             self.moveCounter = 0
         self.checkForMines(self.position.x, self.position.y, data_holder)
-    def isTileFreeForMonster(self, dX, dY, mapList):
+    def isTileFreeForMonster(self, dX, dY, mapList, data_holder):
+        newX = self.position.x + dX
+        newY = self.position.y + dY
         mapTile = mapList[self.position.y + dY][self.position.x + dX]
-        if mapTile.passable and mapTile.char == ',' or mapTile.char == 'O':
+        if mapTile.passable and mapTile.char == ',' or mapTile.char == 'O' and not self.checkForMonster(newX, newY, data_holder.monsters):
             return True
-        elif self.isFlying:
+        elif self.isFlying and not self.checkForMonster(newX, newY, data_holder.monsters):
             return True
         return False
     def checkForMines(self, x, y, data_holder):
@@ -39,3 +41,9 @@ class Monster:
             if m.location.x == x and m.location.y == y and m.alive:
                 self.alive = False
                 m.kill()
+    def checkForMonster(self, x, y, items):
+        for i in items:
+            if i.position.x == x and i.position.y == y:
+                return True
+        return False
+
